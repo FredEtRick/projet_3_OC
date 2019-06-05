0bcea5c7b8379b0b6b864136faf5bbb66ba4b534
travail <travail@macbook-pro-de-frederic.home>
2019-06-05 14:37:07 +0200

    ajout nav en-tête avec liens vers autres sites, pour déploiement

--- view/template.php
+++ view/template.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="../public/plugins/bootstrap/css/bootstrap.css" />
-        <link rel="stylesheet" href="../public/mychanges.css" />
+        <link rel="stylesheet" href="../public/myChanges.css" />
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
         <!--[if lt IE 9]>
         <![endif]-->
@@ -14,6 +14,28 @@
     <body>
         <div class="fondBleu">
             <div class="container">
+                <div class="bg-dark row navperso">
+                    <ul class="col-12 row ulnav">
+                        <li class="col-sm col-12">
+                            <a href="http://frederic-malard.com">
+                                portfolio
+                            </a>
+                        </li>
+                        <li class="col-sm col-12">
+                            <a href="http://louvre.frederic-malard.com">
+                                louvre
+                            </a>
+                        </li>
+                        <li class="col-sm col-12">
+                            <a href="http://blog.frederic-malard.com">
+                                blog personnel
+                            </a>
+                        </li>
+                        <li class="col-sm col-12">
+                            blog écrivain
+                        </li>
+                    </ul>
+                </div>
                 <header class="row">
                     <h1 class="enTete col-xxl-12 col-xl-12 col-sm-12"><a href="index.php">BILLET SIMPLE POUR L'ALASKA</a></h1>
                 </header>
